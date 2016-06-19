99308034deb331b814273dedf3e59a7a559d923f
thomas-yancey <tomyncy618@gmail.com>
2016-06-18 23:56:55 -0400

    start tutorial, create app with webpack config

---
+++ app/index.js
@@ -0,0 +1,34 @@
+var React = require('react')
+var ReactDOM = require('react-dom');
+
+var FriendsContainer = React.createClass({
+  render: function(){
+    var name = "tyler"
+    var friends = ["steve","pete","blaine"]
+    return (
+      <div>
+        <h3>Name: {name} </h3>
+        <ShowList names={friends}/>
+      </div>
+    )
+  }
+});
+
+var ShowList = React.createClass({
+  render: function(){
+    var listItems = this.props.names.map(function(friend){
+      return <li>{friend}</li>;
+    });
+    return (
+      <div>
+        <h3> Friends </h3>
+        <ul>
+          {listItems}
+        </ul>
+      </div>
+    )
+  }
+});
+
+ReactDOM.render(<FriendsContainer />,document.getElementById("app"));
+

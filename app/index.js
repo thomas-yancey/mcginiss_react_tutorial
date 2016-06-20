4845355e5a3f656706637b2dc59a0d4b21618c38
thomas-yancey <tomyncy618@gmail.com>
2016-06-20 02:09:17 -0400

    halfway through tutorial, setup promptcontainer for usernames

--- app/index.js
+++ app/index.js
@@ -1,34 +1,6 @@
 var React = require('react')
 var ReactDOM = require('react-dom');
+var routes = require('./config/routes')
 
-var FriendsContainer = React.createClass({
-  render: function(){
-    var name = "tyler"
-    var friends = ["steve","pete","blaine"]
-    return (
-      <div>
-        <h3>Name: {name} </h3>
-        <ShowList names={friends}/>
-      </div>
-    )
-  }
-});
-
-var ShowList = React.createClass({
-  render: function(){
-    var listItems = this.props.names.map(function(friend){
-      return <li>{friend}</li>;
-    });
-    return (
-      <div>
-        <h3> Friends </h3>
-        <ul>
-          {listItems}
-        </ul>
-      </div>
-    )
-  }
-});
-
-ReactDOM.render(<FriendsContainer />,document.getElementById("app"));
+ReactDOM.render(routes,document.getElementById("app"));
 
